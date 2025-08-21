8d15f8fc4110275e9e654ce35d14b73d43c4a2e8
Seongmin <chr92@naver.com>
2025-08-21 16:23:52 +0900

    feat: read

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,10 +1,13 @@
 export default async function Read({ params }) {
   params = await params;
 
+  const resp = await fetch(`http://localhost:9999/topics/${params.id}`);
+  const topic = await resp.json();
+
   return (
     <>
-      <h2>Read</h2>
-      parameters: {params.id}
+      <h2>{topic.title}</h2>
+      {topic.body}
     </>
   );
 }
