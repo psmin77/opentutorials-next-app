8eb6e95685e1e10abbd61cf38effe1c1aa10f4fe
Seongmin <chr92@naver.com>
2025-08-21 16:46:16 +0900

    feat: create

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,3 +1,43 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Create() {
-  return <>Create!!</>;
+  const router = useRouter();
+
+  return (
+    <>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          const options = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ title, body }),
+          };
+          fetch("http://localhost:9999/topics", options)
+            .then((resp) => resp.json())
+            .then((result) => {
+              console.log(result);
+              const lastId = result.id;
+              router.push(`/read/${lastId}`);
+            });
+        }}
+      >
+        <p>
+          <input type="text" name="title" placeholder="title" />
+        </p>
+        <p>
+          <textarea name="body" placeholder="body" />
+        </p>
+        <p>
+          <input type="submit" value="create" />
+        </p>
+      </form>
+    </>
+  );
 }
