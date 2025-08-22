ce57fff0c4ef9ab819c2d2721a6293a49d779f46
Seongmin <chr92@naver.com>
2025-08-22 15:52:25 +0900

    feat: delete

--- src/app/Control.js
+++ src/app/Control.js
@@ -1,8 +1,9 @@
 "use client";
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 export function Control() {
+  const router = useRouter();
   const params = useParams();
   const id = params.id;
   console.log("Control.js > params.id: ", id);
@@ -18,7 +19,19 @@ export function Control() {
             <Link href={`/update/${id}`}>Update</Link>
           </li>
           <li>
-            <input type="button" value="delete" />
+            <input
+              type="button"
+              value="delete"
+              onClick={() => {
+                const options = { method: "DELETE" };
+                fetch(`http://localhost:9999/topics/${id}`, options)
+                  .then((resp) => resp.json())
+                  .then(() => {
+                    router.push("/");
+                    router.refresh();
+                  });
+              }}
+            />
           </li>
         </>
       ) : null}
